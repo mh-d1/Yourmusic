dad118ad4b991c6a8db24052e9deda700a802162
Mn <mmhdhnfii@gmail.com>
2026-04-01 21:09:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// 1. DATA
 const songs = ["Lagu A", "Lagu B", "Lagu C"];
 
-
+// 2. ELEMENT
 const songList = document.getElementById("songList");
 const menu = document.getElementById("menu");
 const player = document.getElementById("player");
 const title = document.getElementById("title");
 
-
+// 3. FUNCTION
 function openPlayer(song) {
   title.innerText = song;
 
@@ -14,7 +15,7 @@ function openPlayer(song) {
   player.classList.add("active");
 }
 
-
+// 4. RENDER
 songs.forEach((song) => {
   const div = document.createElement("div");
   div.className = "song";
@@ -27,13 +28,13 @@ songs.forEach((song) => {
   songList.appendChild(div);
 });
 
-
+// 5. EVENT
 document.getElementById("back").onclick = () => {
   player.classList.remove("active");
   menu.classList.add("active");
 };
 
-
+// 6. ANIMASI
 document.querySelectorAll("button").forEach(btn => {
   btn.addEventListener("click", () => {
     btn.style.transform = "scale(0.9)";
